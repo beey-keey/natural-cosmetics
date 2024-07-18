685f91cd575421fbad597688f113bfd503a70c0a
Boryana Kircheva (HAU-EDCP) <boryana.kircheva@liebherr.com>
2024-07-18 13:16:09 +0300

    update vue.config.js and gitignore

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/natural-cosmetics/" // Replace 'repository-name' with your GitHub repository name
+      : "/",
+  transpileDependencies: ["vue-clamp", "resize-detector"], // Add any additional dependencies to transpile if needed
+};
