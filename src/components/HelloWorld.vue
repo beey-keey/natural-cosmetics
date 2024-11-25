0181a25c7404b669d1bfff23ebe30a225ee9c4de
Boryana Kircheva (HAU-EDCP) <boryana.kircheva@liebherr.com>
2024-11-25 18:56:19 +0200

    minor

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,14 +4,7 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
-};
-</script>
+<script></script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
