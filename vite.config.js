2130fb4322f72e091633a6e4461507a7aa706b04
Boryana Kircheva (HAU-EDCP) <boryana.kircheva@liebherr.com>
2024-11-29 23:02:42 +0200

    init

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // Map @ to the src directory
+    },
+  },
   build: {
     outDir: "dist", // Specify the output directory for the production build
     assetsDir: "", // Assets directory relative to outDir
