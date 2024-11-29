9d1851dc08e4f14c5aa47a75e7017d788e3eb801
Boryana Kircheva (HAU-EDCP) <boryana.kircheva@liebherr.com>
2024-11-29 22:05:41 +0200

    init

--- src/components/FlowerBloom.vue
+++ src/components/FlowerBloom.vue
@@ -2,6 +2,7 @@
   <v-card style="height: 100%">
     <v-layout style="height: 100%">
       <v-navigation-drawer
+        style="background-color: #737a5d"
         v-model="drawer"
         :rail="rail"
         permanent
@@ -38,7 +39,7 @@
         </v-list>
       </v-navigation-drawer>
       <v-main style="height: 100%">
-        <v-card style="height: 100%">
+        <v-card style="height: 100%" background-color="edf3e9">
           <h3 style="display: flex; justify-content: center">Rhodo beauty</h3>
         </v-card>
       </v-main>
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === "production"
-      ? "/natural-cosmetics/" // Replace 'repository-name' with your GitHub repository name
+      ? "/rhodo-beauty/" // Replace 'repository-name' with your GitHub repository name
       : "/",
   transpileDependencies: ["vue-clamp", "resize-detector"], // Add any additional dependencies to transpile if needed
 };
